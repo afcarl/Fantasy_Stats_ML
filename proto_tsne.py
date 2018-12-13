1f12ec8d47c86ff83aedfbe2eed3056be7ca5fff
tylerharrold <tylerharrold@gmail.com>
2018-12-13 15:12:12 -0500

    Slight improvements to proto_tsne.py

--- proto_tsne.py
+++ proto_tsne.py
@@ -1,6 +1,6 @@
 import tensorflow as tf 
 import pandas as pd 
-
+import testing_tools
 # This will be a simplistic three layer feed forward neural network that will move from our 22 fantasy
 # football datapoints to 11 to 2 
 # the network will be trained via backprop, and will have, as its error function the one specified in 
@@ -18,39 +18,50 @@ class ptsne(object):
 
 		# variables for our ffnn
 		pth = test_values_path + 'test0generation0child0_rbm_1/'
-		df = pd.read_csv(pth + 'W.csv', sep=',' , header=None)
-		self.layer1_W = tf.Variable( df.values, name='layer1_W', dtype=tf.float32)
-		df = pd.read_csv(pth + 'bh_.csv', sep=',' , header=None )
-		self.layer1_bh_ = tf.Variable(df.values, name='layer1_bh_' , dtype=tf.float32)
-		df = pd.read_csv(pth + 'bv_.csv', sep=',' , header=None)
-		self.layer1_bv_ = tf.Variable(df.values, name='layer1_bv_')
+		df = (pd.read_csv(pth + 'W.csv', sep=',' , header=None)).values
+		self.layer1_W = tf.Variable(df, name='layer1_W' , dtype=tf.float32)
+		df = (pd.read_csv(pth + 'bh_.csv', sep=',' , header=None )).values
+		self.layer1_bh_ = tf.Variable(df, name='layer1_bh_' , dtype=tf.float32)
+		df = (pd.read_csv(pth + 'bv_.csv', sep=',' , header=None)).values
+		self.layer1_bv_ = tf.Variable(df, name='layer1_bv_')
 
 		pth = test_values_path + 'test0generation0child0_rbm_2/'
-		df = pd.read_csv(pth + 'W.csv', sep=',' , header=None)
-		self.layer2_W = tf.Variable(df.values, name='layer2_W')
-		pd.read_csv(pth + 'bh_.csv', sep=',' , header=None)
-		self.layer2_bh_ = tf.Variable(df.values, name='layer2_bh_')
-		pd.read_csv(pth + 'bv_.csv', sep=',' , header=None)
-		self.layer2_bv_ = tf.Variable(df.values, name='layer2_bv_')
+		df = (pd.read_csv(pth + 'W.csv', sep=',' , header=None)).values
+		self.layer2_W = tf.Variable(df, name='layer2_W', dtype=tf.float32)
+		df = (pd.read_csv(pth + 'bh_.csv', sep=',' , header=None)).values
+		self.layer2_bh_ = tf.Variable(df, name='layer2_bh_', dtype=tf.float32)
+		df= (pd.read_csv(pth + 'bv_.csv', sep=',' , header=None)).values
+		self.layer2_bv_ = tf.Variable(df, name='layer2_bv_', dtype=tf.float32)
 		
 		pth = test_values_path + 'test0generation0child0_rbm_3/'
 		df = pd.read_csv(pth + 'W.csv', sep=',' , header=None)
-		self.layer3_W = tf.Variable(df.values, name='layer3_W')
+		self.layer3_W = tf.Variable(df.values, name='layer3_W', dtype=tf.float32)
 		df = pd.read_csv(pth + 'bh_.csv', sep=',' , header=None)
-		self.layer3_bh_ = tf.Variable(df.values , name='layer3_bh_')
+		self.layer3_bh_ = tf.Variable(df.values , name='layer3_bh_', dtype=tf.float32)
 		df = pd.read_csv(pth + 'bv_.csv', sep=',' , header=None)
-		self.layer3_bv_ = tf.Variable(df.values , name='layer3_bv_')
+		self.layer3_bv_ = tf.Variable(df.values , name='layer3_bv_', dtype=tf.float32)
 
 		self.X = tf.placeholder(dtype=tf.float32 , name='Dataset')
 
-		self.testfunc = tf.sigmoid(tf.matmul(self.X, self.layer1_W) + self.layer1_bh_)
+		#self.testfunc = tf.sigmoid(tf.matmul(self.X, self.layer1_W) + self.layer1_bh_)
+		self.layer1 = tf.sigmoid(tf.matmul(self.X, self.layer1_W) + tf.transpose(self.layer1_bh_))
+		self.layer2 = tf.sigmoid(tf.matmul(self.layer1 , self.layer2_W) + tf.transpose(self.layer2_bh_))
+		self.layer3 = tf.sigmoid(tf.matmul(self.layer2 , self.layer3_W) + tf.transpose(self.layer3_bh_))
+
+		#self.loss = tf.distributions.kl_divergence(self.X , self.layer3)
 
 	def test(self , dataset):
 		init_op = tf.global_variables_initializer()
 
 		with tf.Session() as sess:
 			sess.run(init_op)
-			sess.run(self.testfunc  , feed_dict={self.X:dataset})
+			#sess.run(self.testfunc  , feed_dict={self.X:dataset})
+			#print(self.layer1_W)
+			#x = (sess.run(self.layer3 , feed_dict={self.X:dataset}).shape)
+			x = sess.run(self.layer3 , feed_dict={self.X:dataset})
+			
+			testing_tools.write_csv(x , 'didthiswork.csv')
+
 			#sess.run(self.layer3_bh_.eval())
 		
 
